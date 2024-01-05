339cd6cfdb4274f544bec0fc9a2aba069b8f02f0
AlonaRahilevych <ragilevic@gmail.com>
2024-01-06 00:00:38 +0100

    Add CTA

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import './cta.scss';
 const CTA = () => {
-  return <div></div>;
+  return (
+    <div className=' section__padding section__margin gpt3__CTA '>
+      <div className='gpt3__CTA-text'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className='gpt3__CTA-button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;

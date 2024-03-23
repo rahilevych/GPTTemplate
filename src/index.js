7a599b8243829351574d4d3e8a397970746c6b88
AlonaRahilevych <ragilevic@gmail.com>
2024-03-23 11:00:48 +0100

    Delete some files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom';
 
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
+import App from './App';
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
